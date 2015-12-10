93bdd9a22018b39a7fa531e01e3c1a7f7ffb1d0f
wozmin <tomaszwozniak.wzm@gmail.com>
2015-12-10 07:21:22 +0100

    last fixes

--- core/tasks.py
+++ core/tasks.py
@@ -1,5 +1,3 @@
-from __future__ import absolute_import
-
 from time import sleep
 from celery import task
 
@@ -10,16 +8,6 @@ def slow_task():
     sleep(60)
 
 
-@task
-def fast_task():
-    print('fast_task')
-
-
 def run(x):
     for i in xrange(x):
         slow_task.delay()
-
-
-def run_fast(x):
-    for i in xrange(x):
-        fast_task.delay()
